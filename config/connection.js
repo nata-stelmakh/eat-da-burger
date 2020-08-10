96b3d0018a46e7cc847ddc9f06673ed40748cd90
Author Name <stelmakh.natallia@gmail.com>
2020-08-10 17:35:12 -0400

    Added handlebars

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "trilogy!",
-  database: "cat_db",
+  password: "Dragonfly89!",
+  database: "burger_db",
 });
 
 // Make connection.
