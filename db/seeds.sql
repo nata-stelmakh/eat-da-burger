10dbcf700adc78bf558f01d42ce2cb85548eff36
Author Name <stelmakh.natallia@gmail.com>
2020-08-10 17:11:24 -0400

    Database set

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name, devoured) VALUES ('Mega Avocado Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Spicy Bacon', false);
-INSERT INTO burgers (name, devoured) VALUES ('Chicken&Waffle',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mega Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken&Waffle',false);
 
