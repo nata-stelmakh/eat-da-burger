c17dbcc14e15a7afd396148248405d91eb0310b4
Author Name <stelmakh.natallia@gmail.com>
2020-08-10 17:08:03 -0400

    Created database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name, devoured) VALUES ('Mega Avocado Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Spicy Bacon', false);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken&Waffle',false);
+
